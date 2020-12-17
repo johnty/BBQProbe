b6347419cf0ee606fd9ec0701c0e3406b9347077
Johnty Wang <johntywang@gmail.com>
2020-12-17 11:03:36 -0800

    updated root readme.md

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
-#some MAX6675 examples with Arduino
+#some MAX6675 examples with Arduino / Teensy
+
+uses Adafruit MAX6675 library (included)
+
+change the pins to ones on your board.
+
+**serialthermocouple**: sends reading via serial port
+
+**thermocouple_esp32**: esp32 version that runs a simple server that allows probing of temperature via http requests
+
+**thermocouple_OLED**: displays  temperature on a ssd1306 based OLED screen
 
 
+
+TODO: delete multiple copies of MAX6675 library and reference root folder
--- thermocouple_OLED.ino
+++
@@ -1,8 +0,0 @@
-quick and dirty arduino based thermocouple bbq probe with ssd1306 based OLED display using the MAX6675 driver chip (now discontinued as of 2016, but still widely available on the internet)
-
-requires Adafruit libraries for OLED
-MAX6675 library included here
-
-used on a teensy LC but should work fine on most arduino-compatible board with enough pins.
-
-see main sketch for SPI pins to drive the thermocouple. does not require hardware SPI port and can use almost any pin for this if you want to hook up extra probes!
